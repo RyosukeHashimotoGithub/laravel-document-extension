2c3d93debd68ff1449e829472d1aecf3bb90dbf9
pylori <is0024xx@gmail.com>
2020-04-26 12:59:09 +0900

    7.x

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,6 +1,5 @@
-// location.href = location.pathname.replace(/\/laravel\/\d\.(\d|x)/, '6.x');
 const version = location.pathname.match(/\d\.(\d|x)/);
-const here = location.pathname.replace(/\/laravel\/\d\.(\d|x)/, '/laravel/6.x');
+const here = location.pathname.replace(/\/laravel\/\d\.(\d|x)/, '/laravel/7.x');
 const redirect = () => location.href = here;
 
-version && version[0] !== '6.x' && redirect();
+version && version[0] !== '7.x' && redirect();
