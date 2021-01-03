755ed0aeced4b04d09ba2e6127ba7ea4113b3932
Ryosuke Hashimoto <38648341+RyosukeHashimotoGithub@users.noreply.github.com>
2021-01-03 18:36:52 +0900

    Update contentScript.ts

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,4 +2,4 @@ const version = location.pathname.match(/\d\.(\d|x)/);
 const here = location.pathname.replace(/\/laravel\/\d\.(\d|x)/, '/laravel/7.x');
 const redirect = () => location.href = here;
 
-version && version[0] !== '7.x' && redirect();
+version && version[0] !== '8.x' && redirect();
